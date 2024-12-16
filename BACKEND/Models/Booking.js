c332511b10ad296ced08c469cc8debd20153ac7e
Edwin C Anto <121566793+Eduzzz03@users.noreply.github.com>
2024-12-16 15:32:05 +0530

    Update Booking.js

--- BACKEND/Models/Booking.js
+++ BACKEND/Models/Booking.js
@@ -1,15 +1,30 @@
-// models/Booking.js
-const mongoose = require('mongoose');
+// // models/Booking.js
+// const mongoose = require('mongoose');
 
+// const bookingSchema = new mongoose.Schema({
+//   userId: { type: String, required: true },
+//   selectedDate: { type: String, required: true },
+//   selectedTime: { type: String, required: true },
+//   selectedCollege: { type: String, required: true },
+//   selectedDepartment: { type: String, required: true },
+//   className: { type: String, required: true },
+//   status: { type: String, default: 'Pending' },
+// });
+
+// module.exports = mongoose.model('Booking', bookingSchema);
+
+
+
+
+// models/Booking.js
 const bookingSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  selectedDate: { type: String, required: true },
-  selectedTime: { type: String, required: true },
-  selectedCollege: { type: String, required: true },
-  selectedDepartment: { type: String, required: true },
-  className: { type: String, required: true },
-  status: { type: String, default: 'Pending' },
+  userId: String,
+  selectedDate: String,
+  selectedTime: String,
+  selectedCollege: String,
+  selectedDepartment: String,
+  className: String,
 });
 
+bookingSchema.index({ selectedDate: 1, selectedTime: 1 }, { unique: true });
 module.exports = mongoose.model('Booking', bookingSchema);
-
